6aca53faf11662ed538be5ee61ad023822d8333b
Iram Liu <64221371+ili23@users.noreply.github.com>
2020-09-30 10:59:34 -0400

    tip calculator
    js attempt

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hello')
+const tipForm = document.getElementById("#tip-form");
+
+tipForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const billAmount = Number(document.querySelector("#bill-amount").value);
+    const tipPercentage = Number(document.querySelector("#tip-amount").value);
+    const tipAmount = document.querySelector("#final-tip");
+
+
+    let bill = Number(billAmount).value;
+    let tipPercent = Number(tipPercentage).value / 100;
+    let final = (bill*tipPercent);
+
+    tipAmount.innerHTML = `${tipPercent}`;
+    tipAmount.getElementsByClassName.display = "block";
+    console.log(final);
+
+});
